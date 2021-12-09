de55a684f8fada2dd719ca4d82a519888d084687
Alexander C <alexander.culum@gmail.com>
2021-12-09 11:13:34 +0100

    Add comments.

--- src/main/java/dev/wcs/api/client/ApiClientSample.java
+++ src/main/java/dev/wcs/api/client/ApiClientSample.java
@@ -12,13 +12,20 @@ public class ApiClientSample {
     public static void main(String[] args) throws ApiException {
         ApiClient apiClient = new ApiClient();
         BookControllerApi bookControllerApi = new BookControllerApi(apiClient);
+
+        // Get all Books
         List<Book> books = bookControllerApi.indexUsingGET();
         System.out.println(books);
+
+        // New Book to be created
         Book book = new Book();
         book.setAuthor("new author");
         book.setTitle("new title");
         book.setDescription("new descr");
+        // Post/Create new Book
         bookControllerApi.createUsingPOST(book);
+
+        // Get all Books again
         books = bookControllerApi.indexUsingGET();
         System.out.println(books);
     }
